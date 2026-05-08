9dadd62848a6efa353f5d37a01ab01d55f5717d3
Parth Patidar <parth1207patidar@gmail.com>
2026-05-08 11:45:43 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import e from "express";
+
+const app = e();
+const PORT = env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "Hello from server",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`http//localhost:${PORT}`);
+});
